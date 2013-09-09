6982628116d8ab7c783b431ea8bc211523a4b54c
Hedley Phillips <info@digitalessence.net>
2013-09-09 14:32:14 +0100

    Added php

---
+++ register-sidebar-widget.php
@@ -0,0 +1,6 @@
+//* Register newsletter widget area
+genesis_register_sidebar( array(
+	'id'		=> 'aboutmebox',
+	'name'		=> __( 'About Me', 'custom-theme' ),
+	'description'	=> __( 'This is the About Me section found under each post.', 'custom-theme' ),
+) );
